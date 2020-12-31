b1c84364bf449580761eba337be8ec70aeec1ff0
iamxingcong <121950263@qq.com>
2020-12-31 16:58:21 +0800

    update

--- hanoi/Home.js
+++ hanoi/Home.js
@@ -1,63 +1,19 @@
- import React, {
-   Component
- } from 'react';
- import {
-   ActivityIndicator,
-   FlatList,
-   Text,
-   View
- } from 'react-native';
+import React from 'react';
+import {
+  TextInput
+} from 'react-native';
 
- export default class Home extends Component {
-   constructor(props) {
-     super(props);
+const Home = () => {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
 
-     this.state = {
-       data: [],
-       isLoading: true
-     };
-   }
+  return (
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+  );
+}
 
-   componentDidMount() {
-     fetch('http://172.20.10.8:3000/api/wp_users')
-       .then((response) => response.json())
-       .then((json) => {
-         this.setState({
-           data: json.message
-         });
-       })
-       .catch((error) => console.error(error))
-       .finally(() => {
-         this.setState({
-           isLoading: false
-         });
-       });
-   }
 
-   componentWillUnmount() {
-     this.setState = function() {
-       return null;
-     };
-   }
-
-   render() {
-     const {
-       data,
-       isLoading
-     } = this.state;
-
-     return (
-       <View style={{ padding: 8   }}>
-            {isLoading ? <ActivityIndicator/> : (
-              <FlatList
-                data={data}
-                keyExtractor={({ user_pass }, index) => user_pass}
-                renderItem={({ item }) => (
-                  <Text>{item.display_name}, {item.user_email}</Text>
-                )}
-              />
-            )}
-          </View>
-     );
-   }
- };
+export default Home;
